541e8f38f66fcace22014dce08c5a7fcd8298162
you21979 <you2197901@gmail.com>
2015-01-17 15:44:56 +0900

    mod

--- example/gateway_marketmaker.js
+++ example/gateway_marketmaker.js
@@ -3,8 +3,15 @@ var GatewayAccount = require('..').GatewayAccount;
 rp.createConnect().then(function(remote){
     var g = new GatewayAccount(remote, 'tokyojpy','r94s8px6kSw1uZ1MV98dhSRTvc6VMPoPcN', 'JPY');
     return g.showMarketMakers('XRP_JPY.r94s8px6kSw1uZ1MV98dhSRTvc6VMPoPcN').then(function(res){
-        console.log(res.bids.slice(0,5))
-        console.log(res.asks.slice(0,5))
+        console.log('XRP/JPY bid top 5 makers')
+        res.bids.slice(0,5).forEach(function(v){
+            console.log("%s %d", v.account, v.count);
+        })
+        console.log('')
+        console.log('XRP/JPY ask top 5 makers')
+        res.asks.slice(0,5).forEach(function(v){
+            console.log("%s %d", v.account, v.count);
+        })
         return remote;
     }).catch(function(e){
         console.log(e)
