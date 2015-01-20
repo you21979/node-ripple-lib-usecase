dada84902935c63333d8ff6566c04fe7e5a9ef45
you21979 <you2197901@gmail.com>
2015-01-21 01:55:30 +0900

    mod

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 exports.TradeWallet = require('./lib/trade_wallet');
 exports.GatewayAccount = require('./lib/gateway_account');
 exports.UserAccount = require('./lib/user_account');
+exports.util = {
+    time : require('./lib/time'),
+}
